9ca3f6cb8af060ba1a82f2ef9ea5804e064cbd2b
SDSD <SDSD@ITEMIZE>
2025-03-13 16:36:05 +0100

    Added

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -3,7 +3,7 @@ import styles from './Button.module.css'
 function Button() {
    
     return (
-        <button className={styles.button}>Click me </button>
+        <button className={styles.button}>This is A button CLick me </button>
     );
 }
 
